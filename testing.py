d403542a0c585b1dc293a64721ae068adf468bc8
Martín Esparza Iaizzo <martinesparza22.me@gmail.com>
2022-10-03 21:14:40 +0200

    Just trying some things out

---
+++ testing.py
@@ -0,0 +1,7 @@
+"""
+Visualization related testing
+author: @mesparza
+created on: 03-10-2022
+"""
+
+from viz import my_function
---
+++ viz/viz.py
@@ -0,0 +1,11 @@
+"""
+Visualization related functions
+author: @mesparza
+created on: 03-10-2022
+"""
+
+
+def my_function(n):
+    out = n + 2
+    return out
+
